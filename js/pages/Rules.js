de90bb5ee6ef11744764a1808f9992cf7f591640
jbfdhgs <Bambaace1@gmail.com>
2025-12-07 08:06:41 -0500

    Update Rules.js

--- js/pages/Rules.js
+++ js/pages/Rules.js
@@ -1,3 +1,5 @@
-Rules: "Beating challenges: For challenges in the top 15, you must have clicks."
-    
+<!DOCTYPE html>
+    <title>"List Rules"</title>
+<h2>"For challenges in the top 25, you need clicks. "<h2>
+<h2>"We accept challenges up to 40 seconds."<h2>    
     
